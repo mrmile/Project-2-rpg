1f83c6fd38005a8dd0c0402a5a10eacbbd6499f2
mrmile <alvaro.milla02@gmail.com>
2022-02-20 13:21:11 +0100

    Added assets for map creation
    El artista tiene que arreglar las siguientes imágenes para poder hacer adecuadamente los tilesets del mapa:
    -abandoned_vehicles
    -city_objects
    -electric_posts
    -electric_stations
    -fuel_tanks
    -helicopter
    -junk
    -junk2
    -roofs

---
+++ Output/Assets/maps/object_img_data/switch_L.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="switch_L" tilewidth="40" tileheight="85" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="40" height="85" source="switch_L.png"/>
+ </tile>
+</tileset>
---
+++ Output/Assets/maps/object_img_data/switch_R.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="switch_R" tilewidth="42" tileheight="80" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="42" height="80" source="switch_R.png"/>
+ </tile>
+</tileset>
