12f97ee3e9e9fdb752043f4cfb61b389b9e94d06
mrmile <alvaro.milla02@gmail.com>
2022-03-17 19:48:23 +0100

    Create base_ground.tsx

---
+++ Output/Assets/maps/base_ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="base_ground" tilewidth="32" tileheight="16" tilecount="176" columns="8">
+ <image source="base_ground.png" width="256" height="352"/>
+</tileset>
