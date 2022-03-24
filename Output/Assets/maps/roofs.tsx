e03ec6cab98bdf6bc2ddbcb677e7c16aef60f8b9
GerardMartinez02 <73245381+GerardMartinez02@users.noreply.github.com>
2022-03-24 11:54:17 +0100

    Tilsets Changes
    Ground, walls, roofs...

--- Output/Assets/maps/roofs.tsx
+++ Output/Assets/maps/roofs.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="roofs" tilewidth="75" tileheight="50" spacing="1" tilecount="30" columns="6">
- <image source="roofs.png" width="475" height="257"/>
+<tileset version="1.2" tiledversion="1.3.5" name="roofs" tilewidth="75" tileheight="50" tilecount="63" columns="9">
+ <image source="roofs.png" width="675" height="350"/>
 </tileset>
---
+++ Output/Assets/maps/roofs_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="roofs_2" tilewidth="75" tileheight="50" tilecount="90" columns="9">
+ <image source="roofs_2.png" width="675" height="500"/>
+</tileset>
