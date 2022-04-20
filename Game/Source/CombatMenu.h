ab43273725ef5a45065e629244a667a5a9535c0e
Taks7 <albramisaplay@gmail.com>
2022-04-20 17:18:21 +0200

    Combat HUD finished
    TODO: Add the player functionality

--- Game/Source/CombatMenu.h
+++ Game/Source/CombatMenu.h
@@ -39,6 +39,28 @@ public:
 
 private:
 
+	SDL_Texture* combatHUD;
+	SDL_Texture* enemyTurn;
+	SDL_Texture* enemyTurn1;
+	SDL_Texture* enemyTurn2;
+	SDL_Texture* enemyTurn3;
+
+	SDL_Texture* combatShoot;
+	SDL_Texture* combatMelee;
+	SDL_Texture* combatItems;
+	SDL_Texture* combatEscape;
+
+	//GUI BUTTONS
+	GuiButton* combatShootGUI;
+	GuiButton* combatMeleeGUI;
+	GuiButton* combatItemsGUI;
+	GuiButton* combatEscapeGUI;
+
+	int delay = 0;
+	int delay2 = 0;
+	int delay3 = 0;
+
+	int buttonClickedFx = 0;
 };
 
 #endif // __SCENE_FOREST_H__
