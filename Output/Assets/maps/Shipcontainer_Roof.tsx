29c599911f3a63b282f0e77b07a1f8b637cba13f
mrmile <alvaro.milla02@gmail.com>
2022-05-13 23:04:26 +0200

    Added last map (tiled)

---
+++ Output/Assets/maps/Shipcontainer_Roof.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Shipcontainer_Roof" tilewidth="75" tileheight="39" spacing="1" tilecount="48" columns="8">
+ <image source="Shipcontainer_Roof.png" width="607" height="239"/>
+</tileset>
---
+++ Output/Assets/maps/ship_walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="ship_walls" tilewidth="75" tileheight="117" tilecount="128" columns="16">
+ <image source="ship_walls.png" width="1250" height="936"/>
+</tileset>
