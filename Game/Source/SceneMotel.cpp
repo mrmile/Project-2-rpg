09114023fdd8773b12932513d96af04ae97661ed
Alejandromo125 <alejandromartinortega@gmail.com>
2022-06-01 20:48:33 +0200

    Removed non used module

--- Game/Source/SceneMotel.cpp
+++ Game/Source/SceneMotel.cpp
@@ -17,7 +17,6 @@
 #include "ModuleParticles.h"
 #include "ModuleFonts.h"
 #include "PauseMenu.h"
-#include "Pathfinding.h"
 #include "ModuleFadeToBlack.h"
 #include "SceneBase.h"
 #include "GameManager.h"
