4b64e310e0e63201dae4bd905c4622778d76f808
Gerard Martinez <gerardmartinez2002@gmail.com>
2022-06-13 02:46:00 +0200

    final boss

--- Game/Source/Final_Boss.h
+++ Game/Source/Final_Boss.h
@@ -20,7 +20,45 @@ public:
 
 private:
 
-	Animation Idle_Enemy;
+	// IDLE
+
+	Animation idleRightAnim_Enemy1;
+	Animation idleLeftAnim_Enemy1;
+	Animation idleDownAnim_Enemy1;
+	Animation idleUpAnim_Enemy1;
+
+	Animation idleUpLeftAnim_Enemy1;
+	Animation idleUpRightAnim_Enemy1;
+	Animation idleDownLeftAnim_Enemy1;
+	Animation idleDownRightAnim_Enemy1;
+
+
+	//WALK
+
+	Animation rightWalkAnim_Enemy1;
+	Animation leftWalkAnim_Enemy1;
+	Animation downWalkAnim_Enemy1;
+	Animation upWalkAnim_Enemy1;
+
+	Animation rightDownWalkAnim_Enemy1;
+	Animation leftDownWalkAnim_Enemy1;
+	Animation rightUpWalkAnim_Enemy1;
+	Animation leftUpWalkAnim_Enemy1;
+
+
+	//ATTACK
+
+	//DIE
+
+	Animation rightDieAnim_Enemy1;
+	Animation leftDieAnim_Enemy1;
+	Animation downDieAnim_Enemy1;
+	Animation upDieAnim_Enemy1;
+
+	Animation rightDownDieAnim_Enemy1;
+	Animation leftDownDieAnim_Enemy1;
+	Animation rightUpDieAnim_Enemy1;
+	Animation leftUpDieAnim_Enemy1;
 	
 	
 	int counter = 0;
